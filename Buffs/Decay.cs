cbe58f9248e56d22cf07267818a26cce08bb4151
Arik <arikcheslog@gmail.com>
2023-11-01 16:16:44 -0700

    Fix decay not working with certain life regen accessories

--- Buffs/Decay.cs
+++ Buffs/Decay.cs
@@ -14,21 +14,8 @@ namespace TheDrowChallenge.Buffs {
 		}
 
 		public override void Update(Player player, ref int buffIndex) {
-			TheDrowChallengePlayer modplayer = player.GetModPlayer<TheDrowChallengePlayer>();
-			if ((player.position.Y + player.height) / 16f <= Main.worldSurface && !player.HasBuff(ModContent.BuffType<Sustainability>())) {
-				if (player.lifeRegen > 0) {
-					player.lifeRegen = 0;
-				}
-				player.lifeRegenTime = 0;
-				modplayer.decay_accleration = Math.Clamp(++modplayer.decay_accleration, 0, 120);
-				player.lifeRegen -= (int)(256 * modplayer.decay_accleration / 120.0);
-				int dustnumber = Dust.NewDust(new Vector2(player.position.X, player.position.Y), 16, 16, DustID.Wraith, player.velocity.X, player.velocity.Y, 100, default, 2.5f);
-				Main.dust[dustnumber].noGravity = true;
-			} else {
-				modplayer.decay_accleration = 0;
-				player.DelBuff(buffIndex);
-				buffIndex--;
-			}
+			int dustnumber = Dust.NewDust(new Vector2(player.position.X, player.position.Y), 16, 16, DustID.Wraith, player.velocity.X, player.velocity.Y, 100, default, 2.5f);
+			Main.dust[dustnumber].noGravity = true;
 		}
 	}
 }
--- TheDrowChallengePlayer.cs
+++ TheDrowChallengePlayer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Terraria;
 using Terraria.DataStructures;
@@ -16,15 +17,27 @@ namespace TheDrowChallenge {
 			return new[] { new Item(ItemID.Torch) };
 		}
 
+		internal int decay_accleration = 0;
 		public override void PreUpdateBuffs() {
 			if ((Player.position.Y + Player.height) / 16f <= Main.worldSurface && !Player.HasBuff(ModContent.BuffType<Sustainability>())) {
 				Player.AddBuff(ModContent.BuffType<Decay>(), 2);  // Needs to be longer than a tick to show up as a debuff.
 			} else {
+				Player.ClearBuff(ModContent.BuffType<Decay>());
 				decay_accleration = 0;
 			}
 		}
 
-		internal int decay_accleration = 0;
+		public override void UpdateBadLifeRegen() {
+			if (Player.HasBuff(ModContent.BuffType<Decay>())) {
+				if (Player.lifeRegen > 0) {
+					Player.lifeRegen = 0;
+				}
+				Player.lifeRegenTime = 0;
+				decay_accleration = Math.Clamp(++decay_accleration, 0, 120);
+				Player.lifeRegen -= (int)(256 * decay_accleration / 120.0);
+			}
+		}
+
 		public override void SaveData(TagCompound tag) {
 			tag.Add("decay_acceleration", decay_accleration);
 		}
